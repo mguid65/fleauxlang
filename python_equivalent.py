c5df81084a9d7e8224937a08221f4cffaf0abf0a
Matthew Guidry <mguid2088@gmail.com>
2024-03-25 12:28:32 -0500

    Fix python equivalent/update fleauxstd

--- python_equivalent.py
+++ python_equivalent.py
@@ -2,25 +2,18 @@ import fleauxstd as fstd
 
 
 def _materialized_rhs_expression_MultiplyPrint_0(x: float, y: float):
-    return (x, y) | fstd.Multiply() | fstd.Println()
+    return ((x, y) | fstd.Multiply(),) | fstd.Println()
 
 
 MultiplyPrint = fstd.make_node(_materialized_rhs_expression_MultiplyPrint_0)
 
 
 def _materialized_rhs_expression_Polynomial_1(x: float):
-    return ((((4, ((((('x:{}', x) | fstd.Printf()), 1, None) | fstd.Slice(), 0) | fstd.Extract())) | MultiplyPrint(), 7) | fstd.Pow()), ((x, 5) | fstd.Pow())) | fstd.Subtract()
+    return (((4, x) | MultiplyPrint(), 7) | fstd.Pow(), ((x, 5) | fstd.Pow())) | fstd.Subtract()
 
-
-class MyLib:
-    x: float = 5
-
-
-print(MyLib.x)
 
 Polynomial = fstd.make_node(_materialized_rhs_expression_Polynomial_1)
 
-4 | Polynomial() | fstd.Println()
-6 | Polynomial() | fstd.Println()
+((4,) | Polynomial(), (6,) | Polynomial()) | fstd.Println()
 
-1 | fstd.In() | fstd.ToNum() | Polynomial() | fstd.Println()
+# (1,) | fstd.In() | fstd.ToNum() | Polynomial() | fstd.Println()
